47bb4984aa0c0ab9b9eeeebdb2700fbb5b0f6d3d
felipeah-dev <andradehernandezfelipe@gmail.com>
2026-04-12 21:21:19 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-[#F5F0E8]">
+      <p className="font-serif text-[#2C2420] text-2xl">Say It</p>
+    </main>
+  );
+}
